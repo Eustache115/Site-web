1faa36894f5b6f6b635c3329438ac76410cf4f6d
lydwine <zlydwine1@gmail.com>
2025-07-24 13:06:06 +0100

    Ajoutons le fichier hapiness.js sur PP-Dev-Imma

---
+++ hapiness.js
@@ -0,0 +1,7 @@
+// Exemple simple : afficher un message d'accueil dans une alerte
+function bienvenue() {
+  alert("Bienvenue sur mon site !");
+}
+
+// Appel de la fonction pour afficher l'alerte
+bienvenue();
